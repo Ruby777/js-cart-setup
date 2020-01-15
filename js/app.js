981a1ec0c500e2be0bb3244f52dca10d51f4f34e
Lucy <lucynn11@gmail.com>
2020-01-15 10:01:45 +0400

    cart totals

--- js/app.js
+++ js/app.js
@@ -23,31 +23,40 @@
                let partPath = fullPath.slice(pos);
 
                 const item ={};
-                item.img = `img-carts${partPath}`;
+                item.img = `img-cart${partPath}`;
 
                 let name = event.target.parentElement.parentElement.nextElementSibling.children[0].children[0].textContent;
                 let price = event.target.parentElement.parentElement.nextElementSibling.children[0].children[1].textContent;
                 let finalPrice = price.slice(1).trim();
 
                 const cartItem = document.createElement('div');
-                cartItem.classList(
+                cartItem.classList.add(
                     'cart-item', 
                     'd-flex',
                     'justify-content-between',
                     'text-capitalize',
                     'my-3');
+
                 cartItem.innerHTML = `
-                 <img src=${item.img} class="img-fluid rounded-circle" id="item-img" alt="">
+                 <img src="${item.img}" class="img-fluid rounded-circle" id="item-img" alt="">
                  <div class="item-text">
-                    <p id="cart-item-title" class="font-weight-bold mb-0">${item.name}</p>
+                    <p id="cart-item-title" class="font-weight-bold mb-0">${name}</p>
                     <span>$</span>
-                    <span id="cart-item-price" class="cart-item-price" class="mb-0">${item.price}</span>
+                    <span id="cart-item-price" class="cart-item-price" class="mb-0">${finalPrice}</span>
                  </div>
                  <a href="#" id='cart-item-remove' class="cart-item-remove">
                     <i class="fas fa-trash"></i>
                  </a>
                  </div> 
                 `;
+                //select cart
+                const cart = document.getElementById('cart');
+                const total = document.querySelector('.cart-total-container');
+                cart.insertBefore(cartItem, total);
+                alert('item added to the cart');
+
+                //showing totals of the cart
+                showTotals();
 
                 console.log(name);
                 console.log(item);
@@ -56,7 +65,27 @@
 
 
             }
-        })
-    })
+        });
+    });
+
+    //showTotals function
+
+    function showTotals(){
+        //console.log("hello");
+        const total = [];
+        const items = document.querySelectorAll(".cart-item-price");
+
+        items.forEach(function(item) {
+            total.push(parseFloat(item.textContent));
+       });
+       console.log(total);
+
+       const totalMoney = total.reduce(function(total, item){
+           total+= item;
+            return total;
+       },0);
+
+       console.log(totalMoney);
+    }
 
 })();
